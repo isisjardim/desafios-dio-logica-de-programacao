8d3ed2fbc0f452729db5fffe0d43c2edaff08f21
Isis Jardim <isinha.jardim@gmail.com>
2025-07-08 15:52:29 -0300

    atualizando o código para switch

--- index.js
+++ index.js
@@ -26,25 +26,34 @@
 // "O Herói de nome **{nome}** está no nível de **{nivel}**"
 
 let nome = "Gandalf";
-let XP = 2000; 
+let XP = 7050; 
 let nivel = "";
 
- if (XP <= 1000) {
-  nivel = "Ferro";
-} else if (XP <= 2000) {
-  nivel = "Bronze";
-} else if (XP <= 5000) {
-  nivel = "Prata";
-} else if (XP <= 7000) {
-  nivel = "Ouro";
-} else if (XP <= 8000) {
-  nivel = "Platina";
-} else if (XP <= 9000) {
-  nivel = "Ascendente";
-} else if (XP <= 10000) {
-  nivel = "Imortal";
-} else {
-  nivel = "Radiante";
+
+switch (true) {
+  case (XP < 1000):
+    nivel = "Ferro";
+    break;
+  case (XP <= 2000):
+    nivel = "Bronze";
+    break;
+  case (XP <= 5000):
+    nivel = "Prata";
+    break;
+  case (XP <= 7000):
+    nivel = "Ouro";
+    break;
+  case (XP <= 8000):
+    nivel = "Platina";
+    break;
+  case (XP <= 9000):
+    nivel = "Ascendente";
+    break;
+  case (XP <= 10000):
+    nivel = "Imortal";
+    break;
+  default:
+    nivel = "Radiante";
 }
 
 console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
